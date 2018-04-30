e378165dd8730d7a0c0e0dffc5da1ee4a3437c59
Matilda <matilda94@gmail.com>
2018-04-30 22:26:39 +0200

    Implemented SubtitleBit

--- src/components/Subtitle.java
+++ src/components/Subtitle.java
@@ -1,9 +1,38 @@
 package components;
 
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.io.Serializable;
 
 public class Subtitle implements Serializable{
 
+    private List<SubtitleBit> subtitleBits;
 
 
+
+    /* -------------------------------------------------------------------
+     * 	Constructor
+     *  ------------------------------------------------------------------*/
+
+    public Subtitle(List<SubtitleBit> subtitleBits) {
+        this.subtitleBits = subtitleBits;
+    }
+
+
+    /* -------------------------------------------------------------------
+     * 	Getters
+     *  ------------------------------------------------------------------*/
+
+    public List<SubtitleBit> getSubtitleBits() {
+        return subtitleBits;
+    }
+    /* -------------------------------------------------------------------
+     * 	Setters
+     *  ------------------------------------------------------------------*/
+
+    public void setSubtitleBits(List<SubtitleBit> subtitleBits) {
+        this.subtitleBits = subtitleBits;
+    }
 }
