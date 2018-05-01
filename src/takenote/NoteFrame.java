0c10c6b1b8aa4d1a77291713e6f48df4cf225457
Matilda <matilda94@gmail.com>
2018-05-01 23:03:28 +0200

    Began working on NoteFrame

--- src/takenote/NoteFrame.java
+++ src/takenote/NoteFrame.java
@@ -8,8 +8,12 @@ import javax.swing.*;
 
 public class NoteFrame extends JFrame {
 
+
+    private Note note;
+
+
     private JFrame window = new JFrame("TakeNote");
-    private JPanel selectionPanel = new JPanel();
+    private JPanel seasonPanel = new JPanel();
     private JPanel subtitlePanel = new JPanel();
 
     private JPanel scenePanel = new JPanel();
@@ -17,6 +21,52 @@ public class NoteFrame extends JFrame {
     private JPanel topPanel = new JPanel();
 
 
-    JSplitPane top = new JSplitPane();
+    public NoteFrame(Note note) {
+        this.note = note;
+        //window.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        testyTest();
+
+    }
+
+
+
+
+
+
+    JSplitPane subtitlesAndNotes = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, subtitlePanel, scenePanel);
+    JSplitPane subtitleNotesAndSeasons = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, seasonPanel, subtitlesAndNotes);
+    JSplitPane topAndBottom = new JSplitPane(JSplitPane.VERTICAL_SPLIT, topPanel, subtitleNotesAndSeasons);
+
+
+
+
+
+
+    public void testyTest() {
+        seasonPanel.setBackground(Color.cyan);
+        subtitlePanel.setBackground(Color.red);
+        scenePanel.setBackground(Color.yellow);
+        topPanel.setBackground(Color.green);
+
+        subtitlesAndNotes.setPreferredSize(new Dimension(100, 550));
+        topPanel.setMinimumSize(new Dimension(0, 100));
+        seasonPanel.setPreferredSize(new Dimension(200, 100));
+        subtitlePanel.setPreferredSize(new Dimension(400, 100));
+
+
+        window.add(topAndBottom);
+
+    }
+
+
+
+
+
+
+
+
 
 }
---
+++ src/takenote/TestNote.java
@@ -0,0 +1,19 @@
+package takenote;
+
+import javax.swing.*;
+
+public class TestNote extends JFrame{
+
+
+    public static void main(String[] args) {
+        Note note = new Note("TestNote");
+        NoteFrame budgetFrame = new NoteFrame(note);
+
+
+
+
+    }
+
+
+
+}
